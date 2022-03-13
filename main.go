e398f2e72eb821abf25d998a1913d33379bc791e
herlianto <85044832+herlianto-github@users.noreply.github.com>
2022-03-13 19:23:21 +0800

    birthdayCakeCandles

--- main.go
+++ main.go
@@ -60,6 +60,24 @@ func miniMaxSum(arr []int32) {
 	fmt.Println("-----------------")
 }
 
+func birthdayCakeCandles(candles []int32) int32 {
+
+	arr := make(map[int32]int32)
+	var result int32
+	for i := 0; i < len(candles); i++ {
+		arr[candles[i]]++
+	}
+
+	for _, value := range arr {
+		if result < value {
+			result = value
+		}
+	}
+
+	return result
+
+}
+
 func main() {
 	fmt.Print("\033[H\033[2J")
 	fmt.Println("nothing just code")
@@ -68,5 +86,6 @@ func main() {
 	miniMaxSum([]int32{1, 2, 3, 4, 5})
 	miniMaxSum([]int32{7, 69, 2, 221, 8974})
 	miniMaxSum([]int32{256741038, 623958417, 467905213, 714532089, 938071625})
-
+	fmt.Println("=========================================================")
+	fmt.Println(birthdayCakeCandles([]int32{3, 2, 1, 3}))
 }
