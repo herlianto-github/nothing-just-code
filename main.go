3906c6444a4dbad173e684bdc93c70999e5599a2
herlianto <85044832+herlianto-github@users.noreply.github.com>
2022-03-13 19:22:17 +0800

    miniMaxSum

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func staircase(n int) {
 	for i := 0; i < n; i++ {
@@ -39,7 +42,31 @@ func staircase(n int) {
 
 }
 
+func miniMaxSum(arr []int32) {
+	var max int64
+	var min int64
+	arrint := []int{}
+	for i := 0; i < len(arr); i++ {
+		arrint = append(arrint, int(arr[i]))
+	}
+	sort.Ints(arrint)
+	for i := 0; i < len(arrint)-1; i++ {
+		min += int64(arrint[i])
+	}
+	for i := 1; i < len(arrint); i++ {
+		max += int64(arrint[i])
+	}
+	fmt.Println("min", min, "max", max)
+	fmt.Println("-----------------")
+}
+
 func main() {
+	fmt.Print("\033[H\033[2J")
 	fmt.Println("nothing just code")
 	staircase(6)
+	fmt.Println("=========================================================")
+	miniMaxSum([]int32{1, 2, 3, 4, 5})
+	miniMaxSum([]int32{7, 69, 2, 221, 8974})
+	miniMaxSum([]int32{256741038, 623958417, 467905213, 714532089, 938071625})
+
 }
