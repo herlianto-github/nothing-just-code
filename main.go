7eb46641e676ab8cfcaf3aa87ea56af6c793d578
herlianto <85044832+herlianto-github@users.noreply.github.com>
2022-03-13 18:52:35 +0800

    staircase

--- main.go
+++ main.go
@@ -2,6 +2,44 @@ package main
 
 import "fmt"
 
+func staircase(n int) {
+	for i := 0; i < n; i++ {
+		for j := i + 1; j <= n; j++ {
+			fmt.Print("#")
+		}
+		fmt.Println()
+	}
+	fmt.Println("-------")
+	for i := 0; i < n; i++ {
+		for j := 0; j <= i; j++ {
+			fmt.Print("#")
+		}
+		fmt.Println()
+	}
+	fmt.Println("-------")
+	for i := 0; i < n; i++ {
+		for j := 0; j < i; j++ {
+			fmt.Print(" ")
+		}
+		for j := i + 1; j <= n; j++ {
+			fmt.Print("#")
+		}
+		fmt.Println()
+	}
+	fmt.Println("-------")
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			fmt.Print(" ")
+		}
+		for j := 0; j <= i; j++ {
+			fmt.Print("#")
+		}
+		fmt.Println()
+	}
+
+}
+
 func main() {
 	fmt.Println("nothing just code")
+	staircase(6)
 }
